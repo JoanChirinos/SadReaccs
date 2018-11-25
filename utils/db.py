e2e853ae87345512966b0db5c004bf9782f6bb08
Susan Lin <slin15@stuy.edu>
2018-11-24 19:03:43 -0500

    s

--- utils/db.py
+++ utils/db.py
@@ -19,7 +19,9 @@ def create_db():
     db.close()
 
 def isUser(user):
-    #returns boolean of whether user is already in the database
+    '''
+    returns boolean of whether user is already in the database
+    '''
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     c.execute("SELECT username FROM users WHERE username = '{0}'".format(user))
@@ -33,7 +35,9 @@ def isUser(user):
 def getPw(user):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    # returns passwords of specific user
+    '''
+    returns passwords of specific user
+    '''
     c.execute("SELECT password FROM users WHERE username = '{0}'".format(user))
     x = c.fetchone()
     db.commit()
@@ -43,7 +47,9 @@ def getPw(user):
 def getSearches(user):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    # returns all searches of a specific user
+    '''
+    returns all searches of a specific user
+    '''
     c.execute("SELECT search FROM searches WHERE username = '{0}'".format(user))
     x = c.fetchall()
     db.commit()
@@ -53,7 +59,9 @@ def getSearches(user):
 def register(user, pw):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    # inputs username and password into the users database
+    '''
+    inputs username and password into the users database
+    '''
     c.execute("INSERT INTO users VALUES('{0}','{1}')".format(user,pw))
     db.commit()
     db.close()
@@ -61,7 +69,9 @@ def register(user, pw):
 def addSearch(user, search):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    # creates entry in searches database of search text, username, and timestamp
+    '''
+    creates entry in searches database of search text, username, and timestamp
+    '''
     c.execute("INSERT INTO searches VALUES ('{0}', '{1}', '{2}')".format(user, search, datetime.utcnow()))
     db.commit()
     db.close()
