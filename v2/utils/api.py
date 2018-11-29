1314cf5a770282eb1b9ef53503ce7e6acdaa2286
raunakchowdhury <rchowdhury5@stuy.edu>
2018-11-28 21:48:43 -0500

    finished api work

--- v2/utils/api.py
+++ v2/utils/api.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request as request
+from datetime import date
 
 def access_info(URL_STUB, API_KEY = None, **kwargs):
     '''
@@ -53,43 +54,122 @@ def search_city(query):
 
 def return_weather(latitude, longitude):
     '''
-    Feeds the lat/long from GeoDB into the Climacell API.
-    Returns a dictionary with different weather metrics (temp, dew pt, etc.).
-    Results are updated hourly.
+    Feeds the lat/long from GeoDB into the Climacell API, which provides a 15-day forecast.
+    Forecasts are updated hourly.
     Limit on API calls: 1000/day
+
+    Returns a dictionary with the accompanying data in this format:
+    - 'date' (YYYY-MM-DD): The date, returned as a list with 16 entries.
+    - 'temp' (C): The temperatures, returned as two nested dictionaries 'max' and 'min' containing lists of 16 entries each.
+    - 'precipitation' (mm): The precipitation, returned as a list with 16 entries.
+    - 'feels_like' (C): What the temp feels like, returned as two nested dictionaries 'max' and 'min' containing lists of 16 entries each.
+    - 'wind_speed' (m/s): What the wind speed is, returned as two nested dictionaries 'max' and 'min' containing lists of 16 entries each.
+
+    NOTE: Index 0 contains the forecast for today
     '''
     # set up headers
     headers = {}
     headers['apikey'] = 'dsZbfxJQ2fyXMLhnDJezjoeQJ77kiqSI'
-    URL = 'https://api2.climacell.co/v2/weather/forecast/hourly?lat={}&lon={}'.format(latitude, longitude)
-    result = access_info(URL, **headers)[0]
-    return result
+    URL = 'https://api2.climacell.co/v2/weather/forecast/daily?lat={}&lon={}'.format(latitude, longitude)
+    result = access_info(URL, **headers)
+
+    # assemble dict
+    forecasts = {
+        'date':[],
+        'precipitation':[],
+        'temp':{
+            'min':[],
+            'max':[],
+        },
+        'feels_like':{
+            'min':[],
+            'max':[],
+        },
+        'wind_speed':{
+            'min':[],
+            'max':[],
+        },
+    }
+
+    #populate dict from result
+    for item in result:
+        # print('\nitem:',item['temp'][0]['observation_time'].split('T')[0])
+        forecasts['date'].append(item['temp'][1]['observation_time'].split('T')[0])
+        forecasts['precipitation'].append(item['precipitation_accumulation']['value'])
+        forecasts['temp']['min'].append(item['temp'][0]['min']['value'])
+        forecasts['temp']['max'].append(item['temp'][1]['max']['value'])
+        forecasts['feels_like']['min'].append(item['feels_like'][0]['min']['value'])
+        forecasts['feels_like']['max'].append(item['feels_like'][1]['max']['value'])
+        forecasts['wind_speed']['min'].append(item['wind_speed'][0]['min']['value'])
+        forecasts['wind_speed']['max'].append(item['wind_speed'][1]['max']['value'])
+
+    return forecasts
 
 def return_historical_weather(latitude, longitude):
     '''
     Feeds the lat/long from GeoDB into the Historical Weather API.
     Returns a dictionary with different weather metrics (temp, dew pt, etc.).
     Limit on API calls: 500/day
+
+    Returns a dictionary with the accompanying data in this format:
+    - 'date' (YYYY-MM-DD): The date, returned as a list with 16 entries.
+    - 'temp' (C): The temperatures, returned as two nested dictionaries 'max' and 'min' containing lists of 16 entries each.
+    - 'today': Data fields for today:
+        - 'precipitation': List of precipitation by hour
+        - 'hour': List of hours recorded
+        - 'wind_speed': List of wind speeds recorded
+        - 'temp': List of temps recorded
+        - 'weather_icon': How the sky looks visually
+    NOTE: Index len(date)-1 contains the forecast for today
     '''
 
     API_KEY = 'a7294c4e9c7e471aa64214148182711'
     # Provides weather data from 11/1 to 11/27
-    URL = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx?format=json&q={},{}&date=2018-11-01&enddate=2018-11-27&key='.format(latitude, longitude)
+    URL = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx?format=json&q={},{}&date=2018-11-20&enddate={}&key='.format(latitude, longitude, str(date.today()))
 
     # Get to the weather data
-    result = access_info(URL, API_KEY)['data']['weather'][0]
-    return result
+    result = access_info(URL, API_KEY)['data']['weather']
 
-def return_zip_code(latitude, longitude):
-    '''
-    Feeds the lat/long from GeoDB into the Historical Weather API.
-    Returns a dictionary with different weather metrics (temp, dew pt, etc.).
-    Limit on API calls: 250/day
-    '''
-    API_KEY = 'Po9k2i9YAbWEjU5Kp0ey6J4ImKSKKAnrWVpXhRyPkt0CesMGE2Sw5TfATIwJ5OzF'
-    URL = 'https://www.zipcodeapi.com/rest/{}/'.format(API_KEY)
+    # assemble dict
+    history = {
+        'date':[],
+        'precipitation':[],
+        'temp':{
+            'min':[],
+            'max':[],
+        },
+        'today':{
+            'hours':[],
+            'temp':[],
+            'precipitation':[],
+            'wind_speed':[],
+            'weather_icon': [],
+        },
+    }
 
+    # populate dict from result
+    for item in result:
+        history['date'].append(item['date'])
+        history['temp']['max'].append(item['maxtempC'])
+        history['temp']['min'].append(item['mintempC'])
+
+        precipitation = 0
+        # Tally up all the precipitation and insert it into history
+        for hourly_item in item['hourly']:
+            precipitation += float(hourly_item['precipMM'])
+
+            # For hour-by-hour weather data on today
+            if item['date'] == str(date.today()):
+                history['today']['hours'].append(hourly_item['time'])
+                history['today']['temp'].append(hourly_item['tempC'])
+                history['today']['precipitation'].append(hourly_item['precipMM'])
+                history['today']['wind_speed'].append(hourly_item['windspeedKmph'])
+                history['today']['weather_icon'].append(hourly_item['weatherIconUrl'][0]['value'])
+
+        history['precipitation'].append(precipitation)
+
+    return history
 
 if __name__ == '__main__':
-    #print(return_weather(15,15))
-    print(return_historical_weather(15,15))
+    print(return_weather(15,15))
+    print(return_historical_weather(40,74))
