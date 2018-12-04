3cd058e672d01da8b6833829054bb45fd395cb91
Susan Lin <slin15@stuy.edu>
2018-12-04 14:04:15 -0500

    updated kwarg

--- v2/utils/db.py
+++ v2/utils/db.py
@@ -12,7 +12,7 @@ def create_db():
     c = db.cursor()               #facilitate db ops
 
     c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS searches(username TEXT, long INTEGER, lat INTEGER, city TEXT, time TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS searches(username TEXT, long INTEGER, lat INTEGER, city TEXT, time TEXT, region TEXT, country TEXT, PRIMARY KEY (long, lat))")
 
     db.commit()
     db.close()
@@ -21,9 +21,10 @@ def isUser(user):
     '''
     returns boolean of whether user is already in the database
     '''
+    command_tuple = (user)
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("SELECT username FROM users WHERE username = '{0}'".format(user))
+    c.execute("SELECT username FROM users WHERE username=?",command_tuple)
     name = c.fetchone()
     db.commit()
     db.close()
@@ -35,9 +36,11 @@ def getPw(user):
     '''
     returns passwords of specific user
     '''
+
+    command_tuple = (user)
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("SELECT password FROM users WHERE username = '{0}'".format(user))
+    c.execute("SELECT password FROM users WHERE username=?", command_tuple)
     password = c.fetchone()
     db.commit()
     db.close()
@@ -52,16 +55,18 @@ def getSearches(user):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
+    command_tuple= (user)
+
     print("THIS WORKS")
-    c.execute("SELECT long FROM searches WHERE username = '{0}'".format(user))
+    c.execute("SELECT long FROM searches WHERE username=?",command_tuple)
     dict['long'] = c.fetchall()
-    c.execute("SELECT lat FROM searches WHERE username = '{0}'".format(user))
+    c.execute("SELECT lat FROM searches WHERE username=?",command_tuple)
     dict['lat'] = c.fetchall()
-    c.execute("SELECT city FROM searches WHERE username = '{0}'".format(user))
+    c.execute("SELECT city FROM searches WHERE username=?",command_tuple)
     dict['city'] = c.fetchall()
-    c.execute("SELECT time FROM searches WHERE username = '{0}'".format(user))
+    c.execute("SELECT time FROM searches WHERE username=?",command_tuple)
     dict['time'] = c.fetchall()
-    print("THIS WORKS")
+
     db.commit()
     db.close()
 
@@ -73,20 +78,26 @@ def register(user, pw):
     '''
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("INSERT INTO users VALUES('{0}','{1}')".format(user,pw))
+
+    command_tuple = (user, pw)
+    c.execute("INSERT INTO users VALUES(?,?)", command_tuple)
+
     db.commit()
     db.close()
     return True
 
-def addSearch(user, long, lat, city):
+def addSearch(user, long, lat, city, region=None, country):
     '''
     creates entry in searches database of longitude, latitude, city name, username, and timestamp
     '''
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    #c.execute("SELECT long FROM searches WHERE username = '{0}' WHERE NOT EXISTS (SELECT * FROM searches WHERE username = '{0}', longitude = '{1}')".format(user, long))
-    c.execute("UPDATE searches SET time = '{2}' WHERE username = '{0}' AND city = '{1}'".format(user, city, datetime.utcnow()))
-    c.execute("INSERT INTO searches SELECT '{0}', '{1}', '{2}', '{3}', '{4}' WHERE NOT EXISTS (SELECT city from searches WHERE username = '{0}' AND city = '{3}')".format(user, long, lat, city, datetime.utcnow()))
+
+    command_tuple = (datetime.utcnow(), user, long, lat)
+    c.execute("UPDATE searches SET time=? WHERE username=? AND long=? AND lat=? ", command_tuple)
+    command_tuple = (user, long, lat, city, datetime.utcnow(), region, country, user, city, long, lat)
+    c.execute("INSERT INTO searches SELECT ?,?,?,?,?,?,? WHERE NOT EXISTS (SELECT city from searches WHERE username=? AND city=? AND long=? AND lat=?)", command_tuple)
+
     db.commit()
     db.close()
 
@@ -96,9 +107,11 @@ def removeSearch(user, long, lat):
     '''
     removes entry in searches database based on username, longitude, and latitude
     '''
+    command_tuple = (user, long, lat)
+
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("DELETE FROM searches WHERE username ='{0}' AND long = '{1}' AND lat = '{2}'".format(user, long, lat))
+    c.execute("DELETE FROM searches WHERE username=? AND long=? AND lat=?", command_tuple)
     db.commit()
     db.close()
 
