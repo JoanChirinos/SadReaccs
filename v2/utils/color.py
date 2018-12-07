560cb905c593114ff3f546c571ea6e747ee97080
JoanChirinos <jchirinos@stuy.edu>
2018-12-06 20:11:42 -0500

    made colors based on time zone

--- v2/utils/color.py
+++ v2/utils/color.py
@@ -2,10 +2,21 @@ from datetime import datetime
 
 COLOR_FILE="./data/colors.csv"
 
-def get_rgb():
+def get_rgb(offset):
+    print('offset: {}'.format(offset))
     now = datetime.now()
+    NY_offset = -5
+    search_offset = offset - NY_offset
+    print('overall offset: {}'.format(search_offset))
+    print('NOW: {}:{}:{}'.format(now.hour, now.minute, now.second))
     midnight = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     seconds = (now - midnight).seconds
+    print('seconds before: {}'.format(seconds))
+    seconds += search_offset * 60 * 60
+    print('seconds after: {}'.format(seconds))
+    seconds %= (60 * 60 * 24)
+    print('modding by: {}'.format(60 * 60 * 24))
+    print('seconds after: {}'.format(seconds))
 
     print('SECONDS: {}'.format(seconds))
     index = int(seconds / 108)
@@ -17,10 +28,13 @@ def get_rgb():
 
         print('LEN OF COLOR LIST: {}'.format(len(color_list)))
 
-        print('Color now: {}'.format(color_list[index]))
+        print('Color now: {}\n\n'.format(color_list[index]))
 
     return color_list[index]
 
 
 if __name__ == '__main__':
-    get_rgb()
+    get_rgb(-3)
+    get_rgb(-8)
+    get_rgb(-5)
+    get_rgb(5)
